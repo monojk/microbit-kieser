fdd1c37c6a76f8593a81e09053eba5167652d603
monojk <jkeinert@web.de>
2020-01-06 11:47:16 +0100

    Update Kieser-Timer.py

--- Kieser-Timer.py
+++ Kieser-Timer.py
@@ -39,35 +39,41 @@ def displayCycle(cycle):
 Images = [Image.CLOCK12, nw, Image.CLOCK3, sw, Image.CLOCK6, Image.CLOCK6, Image.CLOCK6,
           sw, Image.CLOCK3, nw, Image.CLOCK12, Image.CLOCK12]
 
+def sleepFor(millisecs):
+    global startLoopTime
+    sleeptime = startLoopTime - running_time() + millisecs
+    sleep(sleeptime)
+
 def starte():
     global i, sound, Images, startTime, startLoopTime
     i = 0
+    startLoopTime = running_time()
     display.show("0")
-    sleep(500)
+    sleepFor(500)
     display.show(s1)
-    sleep(1000)
+    sleepFor(1500)
     display.show(s2)
-    sleep(1000)         # a total of 3.5 seconds pause (countdown) for preparation
+    sleepFor(2500)         # a total of 3.5 seconds pause (countdown) for preparation
     display.show(Images[0])
     displayCycle(0)
-    sleep(1000)
-    if sound > 0 :
+    sleepFor(3500)
+    if sound > 0:
         music.play(music.BA_DING)
     startTime = running_time()
     startLoopTime = startTime
     display.set_pixel(2, 2, 0)
-    sleep(200)
+    sleepFor(200)
     display.set_pixel(2, 2, 9)
     button_a.was_pressed()
-    if button_b.was_pressed() :
+    if button_b.was_pressed():
         i = -1
 
 sound = 0
 i = -1
 while True:
     startLoopTime = running_time()
-    #gesture = accelerometer.current_gesture()
-    #if gesture == "shake":
+    # gesture = accelerometer.current_gesture()
+    # if gesture == "shake":
     #    sound = 1 - sound   # Toggle between sound and mute
     if button_a.was_pressed():
         starte()
@@ -76,16 +82,16 @@ while True:
         elapsed = int( (running_time() - startTime) / 1000 )
         while True:
             display.scroll(elapsed)
-            if button_a.was_pressed() :
+            if button_a.was_pressed():
                 starte()
                 break
-            if button_b.was_pressed() :
+            if button_b.was_pressed():
                 break
             sleep(100)
 
     if i >= 0:
         imagenr = i % 12
-        if imagenr == 0 or imagenr == 6 :
+        if imagenr == 0 or imagenr == 6:
             display.set_pixel(2, 2, 0)
             sleep(200)
             display.set_pixel(2, 2, 9)
@@ -95,16 +101,15 @@ while True:
     else:
         display.show("0")
 
-    if i < 0 :
+    if i < 0:
         sleep(100)          # shorter reaction time on start-button
-    else :
-        sleeptime = startLoopTime - running_time() + 1000
-        sleep(sleeptime)
+    else:
+        sleepFor(1000)      # 1 second
 
-    if i >= 0 :             # seconds counter
+    if i >= 0:              # seconds counter
         i += 1
 
     if i >= seconds:
-        if sound > 0 :
+        if sound > 0:
             music.play(music.BA_DING)
         i = -1              # reset
